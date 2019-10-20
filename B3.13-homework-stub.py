41c42fe58df46a63d1173635f12c606f5ef47ed4
Korolev Pavel <korolev.vrn@gmail.com>
2019-10-20 21:37:15 +0300

    Определен класс HTML и Tag

--- B3.13-homework-stub.py
+++ B3.13-homework-stub.py
@@ -1,9 +1,80 @@
 class Tag:
-    # ...
+    def __init__(self, tag, is_single=False, klass=None, **kwargs):
+        self.tag = tag
+        self.text = ""
+        self.attributes = {}
+
+        self.is_single = is_single
+        self.children = []
+
+        if klass is not None:
+            self.attributes["class"] = " ".join(klass)
+
+        for attr, value in kwargs.items():
+            if "_" in attr:
+                attr = attr.replace("_", "-")
+            self.attributes[attr] = value
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        pass
+
+    def __iadd__(self, other):
+        self.children.append(other)
+        return self
+
+    def __str__(self):
+        attrs = []
+        for attribute, value in self.attributes.items():
+            attrs.append('%s="%s"' % (attribute, value))
+        attrs = " ".join(attrs)
+
+        if self.children:
+            opening = "<{tag} {attrs}>".format(tag=self.tag, attrs=attrs)
+            if self.text:
+                internal = "%s" % self.text
+            else:
+                internal = ""
+            for child in self.children:
+                internal += str(child)
+            ending = "</%s>" % self.tag
+            return opening + internal + ending
+        else:
+            if self.is_single:
+                return "<{tag} {attrs}/>".format(tag=self.tag, attrs=attrs)
+            else:
+                return "<{tag} {attrs}>{text}</{tag}>".format(
+                    tag=self.tag, attrs=attrs, text=self.text
+                )
 
 
 class HTML:
-    # ...
+    def __init__(self, output=None):
+        self.output = output
+        self.children = []
+
+    def __iadd__(self, other):
+        self.children.append(other)
+        return self
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        if self.output is not None:
+            with open(self.output, "w") as fp:
+                fp.write(str(self))
+        else:
+            print(self)
+
+    def __str__(self):
+        html = "<html>\n"
+        for child in self.children:
+            html += str(child)
+        html += "\n</html>"
+        return html
 
 
 class TopLevelTag:
